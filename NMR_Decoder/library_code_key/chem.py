1c269234eb576d57cee271e116cc721ce675018a
SudeekshaVardhan <svardhan0413@gmail.com>
2025-01-17 14:44:50 -0800

    Add files via upload
    This is part of my initial research of RDKit, a chemistry library for Python. These programs serve as a way for me to both familiarize myself with the RDKit library, which I haven't used before, and also to serve as a key for later reference.

---
+++ NMR_Decoder/library_code_key/chem.py
@@ -0,0 +1,18 @@
+from rdkit import Chem
+
+# Basic chem informatic functionality for working with molecular structures
+# Reads molecule and handles different file formats
+
+'''
+Functionality of the Library
+- Reading and writing mols in various file formats
+- Calculating properties
+- Performing substructure searches
+- Molecular Visualization
+'''
+
+mol = Chem.MolFromSmiles("CC(C(=O)O)N")
+smiles = Chem.MolToSmiles(mol)
+
+print(smiles)
+
