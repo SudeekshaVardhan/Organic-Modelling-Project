19c405081d36c14c795347cc9e243dc98c588e60
SudeekshaVardhan <svardhan0413@gmail.com>
2025-03-04 15:55:29 -0800

    noticed an error with allotropes. To fix it, I am manually entering smiles for these structures. If this does not work, then I might have to create my own file to search if it is not within the data base (like a csv)

--- prototype_3/datascraper.py
+++ prototype_3/datascraper.py
@@ -31,8 +31,13 @@ class Datascraper:
         compound_list.append(self.molecule)
     
     def datascraping(self):
-        data = pcp.get_compounds(self.molecule,'name')[0]
-        mol = Chem.MolFromSmiles(data.canonical_smiles)
+        if self.molecule == "graphene":
+            mol = "C12=C3C4=C5C6=C7C=8C(*)=C5C(=C(*)C4=C(C1=C(*)C9=C%10C2=C%11C%12=C3C6=C%13C%14=C7C=%15C=%16C%17=C%14C%18=C%19C%20=C%21C(=C%22C(C(*)=C%21C(*)=C(C%20=C(C%18=C(*)C%17=C(*)C%23=C(C(=C(C(C%16%23)=C(C%15C8*)*)*)*)*)*)*)=C(C=%24C(=C%11%22)C%10=C%25C(=C9*)C(*)=C(C(=C%25C%24*)*)*)*)C%12=C%19%13)*)*"
+        elif self.molecule == "diamond":
+            pass
+        else:
+            data = pcp.get_compounds(self.molecule,'name')[0]
+            mol = Chem.MolFromSmiles(data.canonical_smiles)
         mol = Chem.AddHs(mol)
         img = Draw.MolToImage(mol)
         img.show() # Display image in local image editor (remove in later versions, or push to seperate GUI)
@@ -125,4 +130,11 @@ class Modelling (Datascraper):
         self.newActor.SetMapper(self.newMapper)
 
     def renWin(self):
-        pass
+        self.newRenderer.AddRenderer(self.newRenderer)
+        self.newRenderer.SetSize(500,800)
+
+        interactor = vtk.vtkRenderWindowInteractor()
+        interactor.SetRenderWindow(self.newRenderer)
+
+        self.newRenderer.Render()
+        interactor.Start()
